65e36ed17f70afca36ea6c6ddea0f59fbbc3f71b
Alexey Sokolov <alexey@ebi.ac.uk>
2025-03-06 10:58:03 +0000

    Adding raw_data and assemblies tables to the data_portal_details page

--- fe/pages/data_portal_details.py
+++ fe/pages/data_portal_details.py
@@ -1,10 +1,12 @@
+from typing import Callable
+
 import dash
 import requests
 import json
 from dash import html, Output, Input, callback, dcc
 import dash_bootstrap_components as dbc
+
 from .data_portal import return_badge_status
-import time
 
 dash.register_page(
     __name__,
@@ -36,18 +38,58 @@ def layout(tax_id=None, **kwargs):
                         id="tabs_card",
                     ),
                     md={"width": 10, "offset": 1},
-                    style={"marginTop": "5px"},
+                    style={"marginTop": "5px", "marginBottom": "15px"},
                 )
             ),
             dcc.Store(id='intermediate-value')
         ]
     )
 
-def return_accession_button(accession: str) -> dbc.Button:
+
+def return_biosamples_accession_button(accession: str) -> dbc.Button:
     return dbc.Button(
         accession,
         outline=True,
         href=f"https://www.ebi.ac.uk/biosamples/samples/{accession}")
+
+
+def return_ena_accession_button(accession: str) -> dbc.Button:
+    return dbc.Button(
+        accession,
+        outline=True,
+        href=f"https://www.ebi.ac.uk/ena/browser/view/{accession}")
+
+
+def return_ftp_download_link(url: str) -> html.Div:
+    links = []
+    for link in url.split(";"):
+        link_name = link.split('/')[-1]
+        links.append(
+            dbc.Button(
+                link_name,
+                outline=True,
+                href=f"https://{link}")
+        )
+    return html.Div(links)
+
+
+def return_table(column_names: list[str], field_names: list[str],
+                 data: list[dict[str, str]],
+                 field_function_mapping: dict[str, Callable]) -> dbc.Table:
+    table_header = [html.Thead(html.Tr([html.Th(value) for value in column_names]))]
+    table_body = [
+        html.Tbody(
+            [html.Tr(
+                [html.Td(
+                    field_function_mapping[field_name](row[field_name]) if
+                    field_name in field_function_mapping
+                    else row[field_name]) for field_name in field_names])
+                for
+                row in data])
+    ]
+    return dbc.Table(table_header + table_body, striped=True, bordered=True,
+                     hover=True, responsive=True)
+
 
 @callback(
     Output("card", "children"),
@@ -60,7 +102,6 @@ def return_accession_button(accession: str) -> dbc.Button:
     ]
 )
 def create_data_portal_record(tax_id):
-    time.sleep(2)
     response = requests.get(
         f"https://aegis-be-1091670130981.europe-west2.run.app/data_portal/{tax_id}"
     ).json()
@@ -95,9 +136,16 @@ def create_data_portal_record(tax_id):
     children.append(desc_list)
 
     tabs = [dbc.Tab(label="Metadata", tab_id="metadata_tab")]
-    if len(response["rawData"]) > 0 or len(response["assemblies"]) > 0:
-        tabs.append(dbc.Tab(label="Data", tab_id="data_tab"))
-    return children, tabs, json.dumps(response["samples"])
+    if len(response["rawData"]) > 0:
+        tabs.append(dbc.Tab(label="Raw Data", tab_id="raw_data_tab"))
+    if len(response["assemblies"]) > 0:
+        tabs.append(dbc.Tab(label="Assemblies", tab_id="assemblies_tab"))
+    agg_data = {
+        "samples": response["samples"],
+        "rawData": response["rawData"],
+        "assemblies": response["assemblies"],
+    }
+    return children, tabs, json.dumps(agg_data)
 
 
 @callback(
@@ -107,22 +155,38 @@ def create_data_portal_record(tax_id):
 )
 def create_tabs(active_tab, agg_data):
     agg_data = json.loads(agg_data)
-    table_header = [
-        html.Thead(html.Tr([html.Th(value) for value in
-                            ["Accession", "Scientific Name", "Common Name", "Sex",
-                             "Organism Part", "Current Status"]]))]
-    table_body = [
-        html.Tbody(
-            [html.Tr(
-                [html.Td(return_accession_button(row["accession"])),
-                 html.Td(row["scientificName"]),
-                 html.Td(row["commonName"]),
-                 html.Td(row["sex"]),
-                 html.Td(row["organismPart"]),
-                 html.Td(return_badge_status(row["trackingSystem"]))])
-                for
-                row in agg_data])
-    ]
-    table = dbc.Table(table_header + table_body, striped=True, bordered=True,
-                      hover=True, responsive=True)
-    return table
+    if active_tab == "metadata_tab":
+        field_function_mapping: dict[str, Callable] = {
+            "accession": return_biosamples_accession_button,
+            "trackingSystem": return_badge_status
+        }
+        return return_table(["Accession", "Scientific Name", "Common Name",
+                             "Sex", "Organism Part", "Current Status"],
+                            ["accession", "scientificName", "commonName", "sex",
+                             "organismPart", "trackingSystem"], agg_data["samples"],
+                            field_function_mapping)
+    elif active_tab == "raw_data_tab":
+        field_function_mapping: dict[str, Callable] = {
+            "run_accession": return_ena_accession_button,
+            "sample_accession": return_ena_accession_button,
+            "experiment_accession": return_ena_accession_button,
+            "study_accession": return_ena_accession_button,
+            "fastq_ftp": return_ftp_download_link
+        }
+        return return_table(["Study Accession", "Sample Accession",
+                             "Experiment Accession", "Run Accession", "FASTQ FTP"],
+                            ["study_accession", "sample_accession",
+                             "experiment_accession", "run_accession", "fastq_ftp"],
+                            agg_data["rawData"], field_function_mapping)
+    else:
+        field_function_mapping: dict[str, Callable] = {
+            "accession": return_ena_accession_button,
+            "study_accession": return_ena_accession_button,
+            "sample_accession": return_ena_accession_button
+        }
+        return return_table(["Accession", "Assembly Name", "Description",
+                             "Study Accession", "Sample Accession", "Version"],
+                            ["accession", "assembly_name",
+                             "description", "study_accession", "sample_accession",
+                             "version"],
+                            agg_data["assemblies"], field_function_mapping)

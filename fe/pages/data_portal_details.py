237b45c7514414191d8b47d5b33cd69a6b8d39af
Alexey Sokolov <alexey@ebi.ac.uk>
2025-03-06 09:45:59 +0000

    Adding metadata table to data_portal_details page

--- fe/pages/data_portal_details.py
+++ fe/pages/data_portal_details.py
@@ -1,6 +1,7 @@
 import dash
 import requests
-from dash import html, Output, Input, callback
+import json
+from dash import html, Output, Input, callback, dcc
 import dash_bootstrap_components as dbc
 from .data_portal import return_badge_status
 import time
@@ -37,14 +38,21 @@ def layout(tax_id=None, **kwargs):
                     md={"width": 10, "offset": 1},
                     style={"marginTop": "5px"},
                 )
-            )
+            ),
+            dcc.Store(id='intermediate-value')
         ]
     )
 
+def return_accession_button(accession: str) -> dbc.Button:
+    return dbc.Button(
+        accession,
+        outline=True,
+        href=f"https://www.ebi.ac.uk/biosamples/samples/{accession}")
 
 @callback(
     Output("card", "children"),
     Output("tabs_header", "children"),
+    Output("intermediate-value", "data"),
     Input("card", "key"),
     running=[
         (Output("tabs_card", "class_name"), "invisible",
@@ -52,7 +60,7 @@ def layout(tax_id=None, **kwargs):
     ]
 )
 def create_data_portal_record(tax_id):
-    time.sleep(5)
+    time.sleep(2)
     response = requests.get(
         f"https://aegis-be-1091670130981.europe-west2.run.app/data_portal/{tax_id}"
     ).json()
@@ -89,12 +97,32 @@ def create_data_portal_record(tax_id):
     tabs = [dbc.Tab(label="Metadata", tab_id="metadata_tab")]
     if len(response["rawData"]) > 0 or len(response["assemblies"]) > 0:
         tabs.append(dbc.Tab(label="Data", tab_id="data_tab"))
-    return children, tabs
+    return children, tabs, json.dumps(response["samples"])
 
 
 @callback(
     Output("tabs_body", "children"),
     Input("tabs_header", "active_tab"),
+    Input("intermediate-value", "data")
 )
-def create_tabs(active_tab):
-    return "This is tab {}".format(active_tab)
+def create_tabs(active_tab, agg_data):
+    agg_data = json.loads(agg_data)
+    table_header = [
+        html.Thead(html.Tr([html.Th(value) for value in
+                            ["Accession", "Scientific Name", "Common Name", "Sex",
+                             "Organism Part", "Current Status"]]))]
+    table_body = [
+        html.Tbody(
+            [html.Tr(
+                [html.Td(return_accession_button(row["accession"])),
+                 html.Td(row["scientificName"]),
+                 html.Td(row["commonName"]),
+                 html.Td(row["sex"]),
+                 html.Td(row["organismPart"]),
+                 html.Td(return_badge_status(row["trackingSystem"]))])
+                for
+                row in agg_data])
+    ]
+    table = dbc.Table(table_header + table_body, striped=True, bordered=True,
+                      hover=True, responsive=True)
+    return table

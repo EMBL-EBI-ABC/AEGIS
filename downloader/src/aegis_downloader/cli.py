64796149a483f26fb4ac74be0d5cc6769b230b66
Alexey Sokolov <alexey@ebi.ac.uk>
2026-05-18 13:31:33 +0100

    Bootstrap aegis-downloader package skeleton

---
+++ downloader/src/aegis_downloader/cli.py
@@ -0,0 +1,3 @@
+def main() -> int:
+    """Placeholder; wired up in Task 21."""
+    return 0
---
+++ downloader/tests/test_smoke.py
@@ -0,0 +1,7 @@
+def test_package_imports():
+    import aegis_downloader
+    assert hasattr(aegis_downloader, "__version__")
+
+
+def test_main_module_executable():
+    from aegis_downloader import __main__  # noqa: F401
